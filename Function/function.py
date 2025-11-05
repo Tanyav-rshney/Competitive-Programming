bd79f3c87b7d1177c247a8b4910417736dcd7f93
Tanyav-rshney <tv728879@gmail.com>
2025-11-05 19:47:29 +0530

    update file

--- Function/function.py
+++ Function/function.py
@@ -1,14 +1,30 @@
 #function with argument
-def goodDay(name): #parameter: name
+def goodDay(name): #argument: name
     print("Hello,"  + name)
 
 goodDay("Kaya")
 
 
 
-def goodDay(name, ending):
+def goodDay(name, ending): # arugements: name, ending
     print("Hello,"  + name)
     print(ending)
 
 goodDay("Kaya", "Thank you")    
 goodDay("Tanay", "Thanks")    
+
+
+#function with return type
+def goodDay(name): 
+    print("Hello,"  + name)
+
+a=goodDay("Kaya")
+print(a) #output: return none
+
+
+def goodDay(name): 
+    print("Hello,"  + name)
+    return "bye"
+
+a=goodDay("Kaya")
+print(a) #output: bye
