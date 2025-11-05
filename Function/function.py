935416035f93deca0dcfc5cc986e013ae74f3f52
Tanyav-rshney <tv728879@gmail.com>
2025-11-05 19:39:20 +0530

    Add new files

---
+++ Function/function.py
@@ -0,0 +1,14 @@
+#function with argument
+def goodDay(name): #parameter: name
+    print("Hello,"  + name)
+
+goodDay("Kaya")
+
+
+
+def goodDay(name, ending):
+    print("Hello,"  + name)
+    print(ending)
+
+goodDay("Kaya", "Thank you")    
+goodDay("Tanay", "Thanks")    
